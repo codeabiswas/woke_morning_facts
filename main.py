5eee95fc421332b032d6930e047fe4d0f21897f5
Andrei Biswas <axb6972@rit.edu>
2019-07-18 19:52:32 -0400

    Can fetch interesting fact based on the date today

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("For Initial commit purposes")
+import requests
+from datetime import date
+
+
+todayMonth = date.today().strftime("%m")
+todayDay = date.today().strftime("%d")
+
+if todayDay[0] == '0':
+    todayDay = todayDay[1]
+
+if todayMonth[0] == '0':
+    todayMonth = todayMonth[1]
+
+# API-Endpoint
+url = "http://numbersapi.com/" + todayMonth + "/" + todayDay + "/date?json"
+
+# Get the response object
+factResponse = requests.get(url = url)
+
+fact = factResponse.json()["text"]
+
+print(fact)
