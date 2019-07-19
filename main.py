c9ae1318835c52cf6cc6982ca9ffeb04ffa7e7bb
Andrei Biswas <axb6972@rit.edu>
2019-07-18 20:46:50 -0400

    Set an alarm in 12-hour format and it produces a fact when it is time for the alarm

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
 import requests
 from datetime import date
+import time
 
-# Fetch today's day and month
-todayDay = date.today().strftime("%d")
-todayMonth = date.today().strftime("%m")
 
-# Format the day and month
-if todayDay[0] == '0':
-    todayDay = todayDay[1]
+def setAlarm(alarmTime):
 
-if todayMonth[0] == '0':
-    todayMonth = todayMonth[1]
+    # Get AM/PM
+    dayOrNight = alarmTime[6:]
+    
+    # Set the alarm time to exact :00 seconds
+    alarmTime = alarmTime[0:5] + ":00 " + dayOrNight
+    
+    # Get the current time
+    currTime = time.strftime("%I:%M:%S %p")
 
-# API-Endpoint
-url = "http://numbersapi.com/" + todayMonth + "/" + todayDay + "/date?json"
+    # Run forever
+    while True:
+        # Until it is time for the alarm, just keep printing the current time
+        if (currTime != alarmTime):
+            currTime = time.strftime("%I:%M:%S %p")
+            print("Current Time: ", currTime)
+            time.sleep(1)
 
-# Get the response object
-factResponse = requests.get(url = url)
+        else:
+            print(fetchFact())
+            currTime = time.strftime("%I:%M:%S %p")
+            
 
-# Fetch the fact from the JSON object
-fact = factResponse.json()["text"]
+def fetchFact():
 
-# Print out the fact
-print(fact)
+    # Fetch today's day and month
+    todayDay = date.today().strftime("%d")
+    todayMonth = date.today().strftime("%m")
+
+    # Format the day and month
+    if todayDay[0] == '0':
+        todayDay = todayDay[1]
+
+    if todayMonth[0] == '0':
+        todayMonth = todayMonth[1]
+
+    # API-Endpoint
+    url = "http://numbersapi.com/" + todayMonth + "/" + todayDay + "/date?json"
+
+    # Get the response object
+    factResponse = requests.get(url = url)
+
+    # Fetch the fact from the JSON object
+    fact = factResponse.json()["text"]
+
+    return fact
+
+if __name__ == "__main__":
+
+    alarmTime = "08:46 PM"
+
+    setAlarm(alarmTime)
