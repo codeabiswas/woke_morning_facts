31f140c07890c8d05615f5f90e2b9d0f6cceea34
Andrei Biswas <axb6972@rit.edu>
2019-08-07 13:11:04 -0400

    Added config.yml file. Added Snooze functionality. Added fade-out stop music.

--- main.py
+++ main.py
@@ -1,59 +1,164 @@
-from datetime import date
-from flask import Flask
-from multiprocessing import Process, Value
+import datetime
 import time
 import requests
 import random
 import vlc
 import pafy
+import yaml
 
 # Author:       Andrei Biswas
 # GitHub:       codeabiswas
 # Email:        petitendian@gmail.com
 
 # Created on:   Tuesday, July 18, 2019
-# Modified on:  Saturday, July 27, 2019
+# Modified on:  Wednesday, August 7, 2019
 
+"""
+    This class is for the YouTube VLC player
+"""
+class vlc_player:
+
+    """
+        This method initializes the VLC media player object
+    """
+    def __init__(self, youtubeURL):
+        
+        # If it is a playlist, get a random video from the playlist
+        try:
+            playlist = pafy.get_playlist(youtubeURL)
+            randomIndex = random.randint(0, len(playlist['items']))
+            best = playlist['items'][randomIndex]['pafy'].getbestaudio()
+        except:
+            # Get the video object
+            video = pafy.new(youtubeURL)
+            # Get the best audio from the youtube URL
+            best = video.getbestaudio()
+        
+        # Fetch a VLC Instance
+        instance = vlc.Instance()
+        # Create a new VLC player
+        self.player = instance.media_player_new()
+        # Get the media object
+        self.media = instance.media_new(best.url)
+        # Get the media objects MRL
+        self.media.get_mrl()
+        # Set the player to play the media object
+        self.player.set_media(self.media)
+    
+    """
+        This method plays the YouTube video
+    """
+    def play(self):
+        
+        # Set media player volume to 100
+        self.player.audio_set_volume(100)
+        # Play the media
+        self.player.play()
+    
+    """
+        This method stops the YouTube video by fading out. 
+    """
+    def stop(self):
+        
+        # Max volume as a counter
+        volumeCounter = 100
+
+        # Until the player is not mute, bring down the volume every 0.25 seconds
+        while volumeCounter != 0:
+            self.player.audio_set_volume(volumeCounter)
+            volumeCounter -= 1
+            time.sleep(0.25)
+
+        # Bring back the volume to 100
+        self.player.audio_set_volume(100)
+        # Stop the media
+        self.player.stop()
+        
+"""
+    Fetch settings from the config.yml file
+"""
+def get_settings():
+
+    # Load all the data into the config variable and return it in a list format
+    with open("config.yml", 'r') as ymlfile:
+        config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+
+    # Check if the user has filled out the appropriate data
+    if config['youtube_url'] == 'your-youtube-url-here':
+        print("Please add a YouTube playlist/video link.")
+        return []
+
+    if config['alarm_time'] == 'your-alarm-time-here':
+        print("Please add an alarm time.")
+        return []
+
+    return [config['youtube_url'], config['snooze_time'], config['alarm_time']]
+    
 """
     Sets the alarm and prints a fun fact when the alarm "rings"
 """
-def setAlarm(alarmTime, youtubeURL):
+def set_alarm(configData):
 
-    # Get AM/PM
-    dayOrNight = alarmTime[6:]
+    # YouTube URL
+    youtubeURL = configData[0]
+    
+    # Snooze Time
+    snoozeTime = configData[1]
+    
+    # Alarm Time
+    alarmTime = configData[2]
     
     # Set the alarm time to exact :00 seconds
-    alarmTime = alarmTime[0:5] + ":00 " + dayOrNight
+    alarmTime = alarmTime[0:5] + ":00"
+
+    # Save the default alarm time
+    defaultAlarmTime = alarmTime
     
     # Get the current time
-    currTime = time.strftime("%I:%M:%S %p")
+    currTime = time.strftime("%H:%M:%S")
+
+    someAlarm = vlc_player(youtubeURL)
 
     # Run forever
     while True:
         # Until it is time for the alarm, just keep printing the current time
         if (currTime != alarmTime):
-            currTime = time.strftime("%I:%M:%S %p")
-            print("Current Time: ", currTime)
             time.sleep(1)
 
         else:
-            print(fetchFact())
-            ringAlarm(youtubeURL)
+            
+            # Play the alarm
+            someAlarm.play()
+            # Ask user for stop alarm or snooze
+            typeOfAlarm = input("Snooze or Stop (Press s for stop, and anything else for snooze)?  ")
 
-            # ringProcess = Process(target=ringAlarm, args=(youtubeURL,))
-            # ringProcess.start()
-
-            # Update the time            
-            currTime = time.strftime("%I:%M:%S %p")
-
+            # If the alarm is to be stopped, set the default alarm time
+            if typeOfAlarm == 's':
+                print(fetch_fact())
+                alarmTime = defaultAlarmTime
+                print("New Alarm Time: {}".format(alarmTime))
+                
+            # Otherwise, delay alarm by snooze time set in config file
+            else:
+                alarmTime = datetime.datetime.strptime(time.strftime("%H:%M:%S"), '%H:%M:%S') + datetime.timedelta(days=0, seconds=(snoozeTime*60))
+                alarmTime = alarmTime.strftime("%H:%M:%S")
+                print("New Alarm Time: {}".format(alarmTime))
+            
+            # "Nicely" stop the alarm
+            someAlarm.stop()
+                
+        # Update the time            
+        currTime = time.strftime("%H:%M:%S")
+        print("Current Time: ", currTime)
+            
 """
     Fetch a fact given the date
 """
-def fetchFact():
+def fetch_fact():
 
     # Fetch today's day and month
-    todayDay = date.today().strftime("%d")
-    todayMonth = date.today().strftime("%m")
+    todayDay = datetime.date.today().strftime("%d")
+    todayMonth = datetime.date.today().strftime("%m")
 
     # Format the day and month
     if todayDay[0] == '0':
@@ -66,49 +171,28 @@ def fetchFact():
     url = "http://numbersapi.com/" + todayMonth + "/" + todayDay + "/date?json"
 
     # Get the response object
-    factResponse = requests.get(url = url)
+    factResponse = requests.get(url=url)
 
     # Fetch the fact from the JSON objectDDidDidDidid
     fact = factResponse.json()["text"]
 
     return fact
 
-
 """
-    Play music for the alarm ringtone
+    This method defines what this program mainly does
 """
-def ringAlarm(youtubeURL):
+def main():
 
-    try:
-        playlist = pafy.get_playlist(youtubeURL)
-        randomIndex = random.randint(0, len(playlist['items']))
-        best = playlist['items'][randomIndex]['pafy'].getbestaudio()
-    except:
-        # Get the video object
-        video = pafy.new(youtubeURL)
-        # Get the best audio from the youtube URL
-        best = video.getbestaudio()
-    
-    # Fetch a VLC Instance
-    instance = vlc.Instance()
-    # Create a new VLC player
-    player = instance.media_player_new()
-    # Get the media object
-    media = instance.media_new(best.url)
-    # Get the media objects MRL
-    media.get_mrl()
-    # Set the player to play the media object
-    player.set_media(media)
+    # Fetch the settings containing all the information
+    configData = get_settings()
 
-    # Start playing the media
-    player.play()
+    # If an error has occured (indicated by empty list), just exit the program
+    if len(configData) == 0:
+        return
+
+    #set_alarm(alarmTime, snoozeTime, youtubeURL)
+    set_alarm(configData)
 
 if __name__ == "__main__":
-
-    # youtubeURL = "https://youtu.be/qQWAicHiVhk"
-    youtubeURL = "https://www.youtube.com/watch?v=qQWAicHiVhk&list=PL7mFHfw12HbzdQ34sS_S2vdZe6vIbwubX"
-
-    # Set the alarm time
-    alarmTime = "08:57 PM"
-
-    setAlarm(alarmTime, youtubeURL)
+    # Run the main program
+    main()
